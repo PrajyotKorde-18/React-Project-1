8f4b7993291f555316365b1648c127165a69c4d3
Prajyot Korde <kordeprajyot@gmail.com>
2025-06-05 13:21:27 +0530

    2nd-change

---
+++ src/Components/MovieCard.jsx
@@ -0,0 +1,25 @@
+function movieCard({movie}) {
+    function OnFavoriteClick() {
+        alert("clicked")
+        
+    }
+    
+    return(
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.url} alt={movie.title} />
+                <div className="movie-overlays">
+                    <button className="favorite-btn" onClick={OnFavoriteClick}>
+                        💝
+                    </button>
+                </div>  
+            </div>
+             <div className="movie-info">
+                    <h3>{movie.title}</h3>
+                    <p>{movie.release_date}</p>
+                </div>
+        </div>
+    );
+}
+
+export default movieCard
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,41 @@
+ import { useState } from "react";
+import MovieCard from "../Components/MovieCard";
+
+ function Home(){
+
+    const [searchQuery,setSearchQuery]=useState("")
+
+    const movies=[
+        {id:1,title:"John Wick",release_Date:"2020"},
+        {id:2,title:"Matrix",release_Date:"2004"},
+        {id:3,title:"Terminator",release_Date:"2003"},
+        {id:4,title:"Avatar",release_Date:"2024"},
+    ];
+
+    const handleSearch=()=>{
+        alert(searchQuery)
+    };
+        
+    return(
+        <div className="home">
+            <form onSubmit={handleSearch} className="search0form" >
+                <input type="text" 
+                placeholder="search for movies ... "
+                className="search-input"
+                value={searchQuery}
+                onChange={(e)=>setSearchQuery(e.target.value)}
+                />
+            <button type="submit">Submit</button>
+            </form>
+
+            <div className="movies-grid">
+                {movies.map((movie)=>(
+                    <MovieCard movie={movie} key={movie.id}/>                 
+                ))}
+
+            </div>
+        </div>
+    );
+ }
+
+ export default Home
